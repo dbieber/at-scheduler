0ff08371c0884781f14f8140b4aa683958844b3b
David Bieber <david810@gmail.com>
2016-03-12 17:19:44 -0800

    Install as root in run-atd

--- tests/run-atd.sh
+++ tests/run-atd.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+sudo python setup.py install
+
 # Start the daemon
 sudo python at/atd.py
+
+at in 1 second 'echo test > test.out'
+# TODO(dbieber): assert no file at /opt/at-scheduler/test.out
+cat /opt/at-scheduler/test.out
 sleep 2
+# TODO(dbieber): assert file at /opt/at-scheduler/test.out
+cat /opt/at-scheduler/test.out
+
+# sudo rm /opt/at-scheduler/test.out
 sudo pkill -F /var/run/at-scheduler.pid
